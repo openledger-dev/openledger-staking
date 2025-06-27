e8f9d4aab3b50a99bcf07f94bc7a3eab3a1fc1fb
Alan WANG <alan1995wang@outlook.com>
2025-06-27 12:00:07 +0800

    feat: setup init

---
+++ src/BaseDistributor.sol
@@ -0,0 +1,163 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import "solady/utils/ECDSA.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {OwnableRoles} from "solady/auth/OwnableRoles.sol";
+
+/// @title  Base Distributor
+/// @author Clique (@Clique2046)
+/// @author Alan (@alannotnerd)
+contract BaseDistributor is OwnableRoles {
+    using SafeERC20 for IERC20;
+
+    // address signing the claims
+    address public signer;
+    // vault address
+    address public vault;
+    // whether the airdrop is active
+    bool public active = false;
+
+    address public token;
+
+    mapping(bytes32 => mapping(address => uint256)) public claimed;
+    mapping(bytes32 => bytes32) public merkleConfiguration;
+
+    // errors
+    error AlreadyClaimed();
+    error InvalidSignature();
+    error NotActive();
+    error ZeroAddress();
+    error InvalidMerkleProof(bytes32 root);
+    error WithdrawFailed();
+    error IncorrectFee();
+
+    event AirdropClaimed(address indexed account, bytes32 indexed root, uint256 amount);
+    event MerkleConfigurationUpdate(bytes32 indexed root, bytes32 indexed configurator);
+    event Withdraw(address indexed to, uint256 amount);
+    event FeeSet(uint256 fee);
+    event VaultSet(address indexed vault);
+    event TokenSet(address indexed token);
+
+    uint256 public constant PROJECT_ADMIN = _ROLE_0;
+
+    /// @notice Construct a new Claim contract
+    /// @param _signer address that can sign messages
+    /// @param _projectAdmin address that can set the signer and claim root
+    constructor(address _signer, address _projectAdmin, address _vault) {
+        signer = _signer;
+        vault = _vault;
+        _initializeOwner(msg.sender);
+        _setRoles(_projectAdmin, PROJECT_ADMIN);
+    }
+
+    /// @notice Modifier to check if the airdrop is active
+    modifier whenActive() {
+        if (!active) revert NotActive();
+        _;
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                      ADMIN FUNCTIONS                       */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @notice Set the signer
+    /// @param _signer address that can sign messages
+    function setSigner(address _signer) external onlyRoles(PROJECT_ADMIN) {
+        signer = _signer;
+    }
+
+    /// @notice Set the claim root
+    /// @param _claimRoot root of the merkle tree
+    function setClaimRoot(bytes32 _claimRoot, bytes32 _configurator) external onlyRoles(PROJECT_ADMIN) {
+        merkleConfiguration[_claimRoot] = _configurator;
+        emit MerkleConfigurationUpdate(_claimRoot, _configurator);
+    }
+
+    /// @notice Set the vault
+    /// @param _vault address of the vault
+    function setVault(address _vault) external onlyRoles(PROJECT_ADMIN) {
+        vault = _vault;
+        emit VaultSet(_vault);
+    }
+
+    /// @notice Toggle the active state
+    function toggleActive() external onlyRoles(PROJECT_ADMIN) {
+        active = !active;
+    }
+
+    /// @notice Set the token
+    /// @param _token address of the token
+    function setToken(address _token) external onlyRoles(PROJECT_ADMIN) {
+        token = _token;
+        emit TokenSet(_token);
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                      FEE FUNCTIONS                         */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+    /// @notice Withdraw the fee
+    /// @dev Only callable by the owner
+    function withdrawFee(address _recipient) external onlyOwner {
+        uint256 _balance = address(this).balance;
+        if (_balance > 0) {
+            (bool _success,) = payable(_recipient).call{value: _balance}("");
+            if (!_success) revert WithdrawFailed();
+            emit Withdraw(_recipient, _balance);
+        }
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                   EXTERNAL FUNCTIONS                       */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @notice Claim airdrop tokens. Checks for both merkle proof
+    //          and signature validation
+    /// @param _signature signature of the claim
+    /// @param _amount amount of tokens to claim
+    /// @param _onBehalfOf address to claim on behalf of
+    function claim(
+        bytes calldata _signature,
+        uint256 _amount,
+        address _onBehalfOf
+    ) external payable whenActive {
+        address _signer = signer;
+
+        // if the signer is not set, skip signature check
+        if (_signer != address(0)) {
+            _signatureCheck(_amount, _onBehalfOf, _signature, bytes32(0), _signer);
+        }
+
+        IERC20(token).safeTransferFrom(vault, _onBehalfOf, _amount);
+
+        emit AirdropClaimed(_onBehalfOf, bytes32(0), _amount);
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                       PRIVATE FUNCTIONS                    */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+
+    /// @notice Internal function to check the signature
+    /// @param _amount amount of tokens to claim
+    /// @param _onBehalfOf address to claim on behalf of
+    /// @param _signature signature of the claim
+    /// @param _root root of the merkle tree
+    /// @param _signer signer to check
+    function _signatureCheck(
+        uint256 _amount,
+        address _onBehalfOf,
+        bytes calldata _signature,
+        bytes32 _root,
+        address _signer
+    ) internal view {
+        if (_signature.length == 0) revert InvalidSignature();
+
+        bytes32 messageHash = keccak256(abi.encodePacked(_onBehalfOf, _amount, _root, address(this), block.chainid));
+        bytes32 prefixedHash = ECDSA.toEthSignedMessageHash(messageHash);
+        address recoveredSigner = ECDSA.recoverCalldata(prefixedHash, _signature);
+
+        if (recoveredSigner != _signer) revert InvalidSignature();
+    }
+}
---
+++ src/Staking.sol
@@ -0,0 +1,399 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import {OwnableRoles} from "solady/auth/OwnableRoles.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {FixedPointMathLib} from "solady/utils/FixedPointMathLib.sol";
+import {EIP712} from "solady/utils/EIP712.sol";
+import {ECDSA} from "solady/utils/ECDSA.sol";
+
+/// @title Staking Contract
+/// @notice A flexible staking system that allows users to stake tokens and earn interest
+/// @dev Implements EIP-712 for typed data signing and uses Solady's OwnableRoles for access control
+struct Stake {
+    address recipient; // Commitment to the stake
+    uint256 configId; // Configuration ID
+    uint256 claimAt; // Timestamp of the last interest claim
+    uint256 accruedInterest; // Accumulated interest that hasn't been claimed
+    uint256 amount; // Amount of tokens staked
+    uint256 startTime; // Timestamp when the stake was created
+}
+
+struct StakeCommitment {
+    bytes32 commitment;
+    bytes signature;
+}
+
+/// @notice Configuration for a stake type
+struct StakeConfig {
+    address manager; // Address that can manage the stake
+    address bank; // Address that holds the staked tokens
+    address token; // The token that users can stake
+    uint256 interestRate; // Interest rate per second (in wei)
+    uint256 stakeDuration; // Duration of the stake in seconds
+    uint256 cooldownDuration; // Duration of the cooldown period in seconds
+
+    uint256 maxStake; // Maximum amount of tokens that can be staked
+    uint256 minStake; // Minimum amount of tokens that can be staked
+    bool isActive; // Whether the stake is active
+    bool isTopupEnabled; // Whether the stake is topped up
+    bool enbaleCommit; // Whether the stake supports commitment
+}
+
+/// @notice Represents an unstake request with timing information
+struct UnstakeRequest {
+    uint256 requestAt; // Timestamp when the unstake was requested
+    Stake stake; // The stake being unstaked
+}
+
+/// @title Staking
+/// @notice A flexible staking system that allows users to stake tokens and earn interest
+contract Staking is EIP712 {
+    using SafeERC20 for IERC20;
+    using FixedPointMathLib for uint256;
+
+    /// @notice Mapping of stake ID to stake information
+    mapping(uint256 => Stake) public stakes;
+    mapping(uint256 => StakeCommitment) public stakeCommitments;
+    /// @notice Counter for generating unique stake IDs
+    uint256 public nextStakeId;
+
+    /// @notice Mapping of stake ID to unstake requests
+    mapping(uint256 => UnstakeRequest) public unstakeRequests;
+
+    /// @notice Mapping of configuration hash to enabled features
+    mapping(uint256 => StakeConfig) public configs;
+    mapping(uint256 => mapping(address => uint256)) public stakedAmounts;
+
+    event Staked(
+        uint256 indexed stakingId,
+        address indexed recipient,
+        uint256 indexed amount
+    );
+    event ToppedUp(
+        uint256 indexed stakingId,
+        address indexed recipient,
+        uint256 amount
+    );
+    event Unstaked(uint256 indexed stakingId, address indexed recipient);
+    event RequestUnstake(uint256 indexed stakingId, address indexed recipient);
+    event ConfigSet(uint256 indexed configId);
+    event CooldownDurationSet(uint256 cooldownDuration);
+    event TokenAddressesSet(address token);
+    event CommitStake(uint256 indexed stakingId, bytes32 indexed commitment);
+
+    error InactiveConfigOrInvalidSender();
+    error StakeNotFound();
+    error UnstakeRequestNotFound();
+    error CooldownNotPassed();
+    error ZeroAmount();
+    error StakeNotEnded();
+    error StakeEnded();
+    error StakeAmountExceeded();
+    error StakeAmountTooSmall();
+    error MismatchedRecipient();
+    error Unauthorized();
+
+    /// @notice Creates a new staking contract
+    /// @dev Initializes the contract with the deployer as the owner
+    constructor() {}
+
+    /// @notice Ensures the amount is greater than zero
+    /// @param _amount The amount to check
+    modifier nonZeroAmount(uint256 _amount) {
+        if (_amount == 0) {
+            revert ZeroAmount();
+        }
+        _;
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                     Admin Functions                        */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    function setConfig(
+        uint256 _configId,
+        StakeConfig calldata _config
+    ) external {
+        if (_config.manager != msg.sender) {
+            revert Unauthorized();
+        }
+        configs[_configId] = _config;
+        emit ConfigSet(_configId);
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                     External Functions                     */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @notice Allows a user to stake tokens
+    /// @param _configId The configuration for the stake (duration, interest rate, whitelist)
+    /// @param _onBehalfOf The address that will receive the staked tokens and interest
+    /// @param _amount The amount of tokens to stake
+    /// @dev SECURITY:
+    /// @dev - Front-running: Safe because token transfer is from msg.sender
+    /// @dev - Reentrancy: Safe because transfer is done after state mutation
+    /// @dev - Access control: Checks whitelist if configured
+    function stake(
+        uint256 _configId,
+        address _onBehalfOf,
+        uint256 _amount
+    ) external nonZeroAmount(_amount) {
+        StakeConfig memory config_ = configs[_configId];
+
+        if (!config_.isActive) {
+            revert InactiveConfigOrInvalidSender();
+        }
+
+        IERC20(config_.token).safeTransferFrom(
+            msg.sender,
+            config_.bank,
+            _amount
+        );
+
+        if (!config_.isActive) {
+            revert InactiveConfigOrInvalidSender();
+        }
+
+        Stake memory stake_ = Stake({
+            recipient: _onBehalfOf,
+            configId: _configId,
+            claimAt: block.timestamp,
+            accruedInterest: 0,
+            amount: _amount,
+            startTime: block.timestamp
+        });
+
+        stakes[nextStakeId++] = stake_;
+        stakedAmounts[_configId][_onBehalfOf] += _amount;
+
+        if (stakedAmounts[_configId][_onBehalfOf] < config_.minStake) {
+            revert StakeAmountTooSmall();
+        }
+
+        if (stakedAmounts[_configId][_onBehalfOf] > config_.maxStake) {
+            revert StakeAmountExceeded();
+        }
+
+        emit Staked(nextStakeId, _onBehalfOf, _amount);
+    }
+
+    function commitStake(bytes32 _commitment, bytes calldata _permit) external {
+        StakeCommitment memory commitment_ = StakeCommitment({
+            commitment: _commitment,
+            signature: _permit
+        });
+
+        stakeCommitments[nextStakeId++] = commitment_;
+        emit CommitStake(nextStakeId, _commitment);
+    }
+
+    /// @notice Allows topping up an existing stake
+    /// @param _stakingId The ID of the stake to top up
+    /// @param _amount The amount of tokens to add to the stake
+    /// @dev SECURITY:
+    /// @dev - Reentrancy: Safe because transfer is done after state mutation
+    /// @dev - Access control: Only allowed if TOPUP_ENABLED is set for the stake config
+    function topUpStake(
+        uint256 _stakingId,
+        uint256 _amount
+    ) external nonZeroAmount(_amount) {
+        Stake memory stake_ = stakes[_stakingId];
+        StakeConfig memory config_ = configs[stake_.configId];
+
+        if (!config_.isTopupEnabled || stake_.recipient != msg.sender) {
+            revert InactiveConfigOrInvalidSender();
+        }
+
+        if (stake_.amount == 0) {
+            revert StakeNotFound();
+        }
+
+        IERC20(config_.token).safeTransferFrom(
+            msg.sender,
+            config_.bank,
+            _amount
+        );
+
+        uint256 interest = calculateInterest(stake_);
+
+        stake_.amount += interest + _amount;
+        stake_.startTime = block.timestamp;
+        stake_.claimAt = block.timestamp;
+        stake_.accruedInterest = 0;
+
+        stakes[_stakingId] = stake_;
+
+        emit ToppedUp(_stakingId, stake_.recipient, _amount);
+    }
+
+    function requestUnstakeWithCommitment(
+        uint256 _stakingId,
+        uint256 _configId,
+        uint256 _startTime,
+        uint256 _amount
+    ) external {
+        StakeCommitment memory commitment_ = stakeCommitments[_stakingId];
+        if (commitment_.commitment == 0) {
+            revert StakeNotFound();
+        }
+
+        bytes32 digest = _hashTypedData(keccak256(abi.encode(
+            keccak256("Stake(uint256 configId,uint256 amount,uint256 startTime)"),
+            _configId,
+            _startTime,
+            _amount
+        )));
+
+        StakeConfig memory config_ = configs[_configId];
+
+        address signer = ECDSA.recover(digest, commitment_.signature);
+
+        if (signer != config_.manager) {
+            revert Unauthorized();
+        }
+
+        // Open commitment
+        delete stakeCommitments[_stakingId];
+        Stake memory stake_ = Stake({
+            recipient: msg.sender,
+            configId: _configId,
+            claimAt: _startTime,
+            accruedInterest: 0,
+            amount: _amount,
+            startTime: _startTime
+        });
+        stakes[_stakingId] = stake_;
+        stakedAmounts[_configId][msg.sender] += _amount;
+
+        this.requestUnstake(_stakingId);
+    }
+
+    /// @notice Initiates the unstaking process for a stake
+    /// @param _stakingId The ID of the stake to unstake
+    /// @dev If cooldown is not required, unstakes immediately
+    /// @dev If cooldown is required, creates an unstake request
+    function requestUnstake(uint256 _stakingId) external {
+        Stake memory stake_ = stakes[_stakingId];
+        if (stake_.amount == 0) {
+            revert StakeNotFound();
+        }
+
+        if (stake_.recipient != msg.sender) {
+            revert MismatchedRecipient();
+        }
+
+        stake_ = accureInterest(stake_);
+        delete stakes[_stakingId];
+
+        StakeConfig memory config_ = configs[stake_.configId];
+
+        if (config_.cooldownDuration == 0) {
+            inner_unstake(_stakingId, stake_);
+        } else {
+            unstakeRequests[_stakingId] = UnstakeRequest({
+                requestAt: block.timestamp,
+                stake: stake_
+            });
+            emit RequestUnstake(_stakingId, stake_.recipient);
+        }
+    }
+
+    /// @notice Completes the unstaking process after cooldown period
+    /// @param _stakingId The ID of the stake to unstake
+    /// @dev Can only be called after the cooldown period has passed
+    function unstake(uint256 _stakingId) external {
+        UnstakeRequest memory unstakeRequest_ = unstakeRequests[_stakingId];
+        StakeConfig memory config_ = configs[unstakeRequest_.stake.configId];
+        if (unstakeRequest_.requestAt == 0) {
+            revert UnstakeRequestNotFound();
+        }
+
+        if (unstakeRequest_.stake.recipient != msg.sender) {
+            revert MismatchedRecipient();
+        }
+
+        if (
+            block.timestamp <
+            unstakeRequest_.requestAt + config_.cooldownDuration
+        ) {
+            revert CooldownNotPassed();
+        }
+        delete unstakeRequests[_stakingId];
+        inner_unstake(_stakingId, unstakeRequest_.stake);
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                     Internal Functions                     */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @notice Internal function to handle unstaking logic
+    /// @param _stakeId The ID of the stake to unstake
+    /// @param _stake The stake information
+    function inner_unstake(uint256 _stakeId, Stake memory _stake) internal {
+        StakeConfig memory config_ = configs[_stake.configId];
+        bool isEnded_ = _stake.startTime + config_.stakeDuration <=
+            block.timestamp;
+        if (!isEnded_) {
+            revert StakeNotEnded();
+        }
+
+        stakedAmounts[_stake.configId][_stake.recipient] -= _stake.amount;
+
+        IERC20(config_.token).safeTransferFrom(
+            config_.bank,
+            msg.sender,
+            _stake.amount + _stake.accruedInterest
+        );
+        emit Unstaked(_stakeId, msg.sender);
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                       View Functions                       */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @notice Calculates and accrues interest for a stake
+    /// @param _stake The stake to accrue interest for
+    /// @return _stake The stake with accrued interest
+    function accureInterest(
+        Stake memory _stake
+    ) public view returns (Stake memory) {
+        uint256 interest = calculateInterest(_stake);
+        _stake.accruedInterest = interest;
+        _stake.claimAt = block.timestamp;
+        return _stake;
+    }
+
+    /// @notice Calculates the interest earned for a stake
+    /// @param _stakeInfo The stake information
+    /// @return uint256 The amount of interest earned
+    function calculateInterest(
+        Stake memory _stakeInfo
+    ) public view returns (uint256) {
+        uint256 claimAt_ = _stakeInfo.claimAt;
+        StakeConfig memory config_ = configs[_stakeInfo.configId];
+        uint256 stakeDuration_ = config_.stakeDuration;
+        uint256 interestRate_ = config_.interestRate;
+        uint256 amount_ = _stakeInfo.amount;
+
+        uint256 elapsedTime_ = block.timestamp - claimAt_;
+        uint256 upperBound_ = (stakeDuration_ == 0)
+            ? type(uint256).max
+            : stakeDuration_;
+        elapsedTime_ = elapsedTime_ > upperBound_ ? upperBound_ : elapsedTime_;
+
+        uint256 interest_ = amount_.mulWad(interestRate_ * elapsedTime_);
+        return interest_ + _stakeInfo.accruedInterest;
+    }
+
+    function _domainNameAndVersion()
+        internal
+        view
+        virtual
+        override
+        returns (string memory name, string memory version)
+    {
+        return ("Staking", "1");
+    }
+}
